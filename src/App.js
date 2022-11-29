a8b986454ab0acb2fa45eb755ad5ce89e425790a
explodingpineapple41 <47233722+explodingpineapple41@users.noreply.github.com>
2022-11-29 17:45:36 -0500

    Added WIP navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
+import Navbar from './navbar/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Navbar />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +19,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ src/navbar/Navbar.js
@@ -0,0 +1,28 @@
+import '../App.css';
+import {ReactComponent as Logo} from "../media/respawn_logo.svg"
+
+function Navbar() {
+    return (
+        <nav class="navbar">
+            <div class="navbar-section" id="navbar-logo">
+                <a href="/">
+                    <Logo />
+                </a>
+            </div>
+            <div class="navbar-section" id="navbar-about">
+                <a href="/" class="nav-link">ABOUT</a>
+            </div>
+            <div class="navbar-section" id="navbar-first">
+                <a href="/" class="nav-link"><i>FIRST</i></a>
+            </div>
+            <div class="navbar-section" id="navbar-sponsors">
+                <a href="/" class="nav-link">SPONSORS</a>
+            </div>
+            <div class="navbar-section" id="navbar-contact">
+                <a href="/" class="nav-link">CONTACT</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
