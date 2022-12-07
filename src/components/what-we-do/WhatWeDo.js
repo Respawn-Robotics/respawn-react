08a8cafe241af4611164dbb559a389e3aa8a3102
devbhagat5566 <99219039+devbhagat5566@users.noreply.github.com>
2022-12-07 13:48:30 -0500

    fixed navbar bug + working on home page 'What we do' component

---
+++ src/components/what-we-do/WhatWeDo.js
@@ -0,0 +1,16 @@
+import './what-we-do.css';
+
+function WhatWeDo() {
+    return(
+        <div className='what-we-do'>
+            <div className='what-we-do-square'></div>
+            <div className='what-we-do-square'></div>
+            <div className='what-we-do-square'></div>
+            <div className='what-we-do-square'></div>
+            <div className='what-we-do-square'></div>
+            <div className='what-we-do-square'></div>
+        </div>
+    );
+}
+
+export default WhatWeDo;
