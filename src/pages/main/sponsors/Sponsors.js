91f89c5b0cf629798cb09a5203495d7a0d9f3567
AstonFrazier3218 <Aston.Frazier3218@Butlertech.org>
2022-12-07 16:44:58 -0500

    Added sponsors page

---
+++ src/pages/main/sponsors/Sponsors.js
@@ -0,0 +1,31 @@
+import './sponsors.css';
+import React from 'react';
+
+function Sponsors() {
+    return (
+    <>
+        <section className='sponsors-container'>
+            <div className='sponsors-section1' id='container'>
+                <p className='sponsors-text'>
+                Our Sponsors
+                </p>
+            </div>
+            <div className='sponsors-section2' id='containter'>
+                <h2 className='sponsors-text'>
+                Why Sponsor?
+                </h2>
+                <p className='sponsors-text'>
+                Sponsoring the team helps us change the lives of our students. Your money will go toward operating costs, competition entry fees, travel costs, and much more.
+                </p>
+            </div>
+            <div className ='sponsors-section3' id='container'>
+                <h2 className='sponsors-text'>
+                    Uwu Sponsor name
+                </h2>
+            </div>
+        </section>
+    </>
+  );
+}
+
+export default Sponsors;
