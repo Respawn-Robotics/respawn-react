f39b74165bb9b6d2318b6caf400d4eb86c3a68b7
Dru Frazier <drufrazier05@gmail.com>
2022-12-21 16:04:37 -0500

    added small cosmetic tweaks

--- src/pages/main/home/Home.js
+++ src/pages/main/home/Home.js
@@ -35,13 +35,13 @@ function Home() {
     <>
       <div className='image-slider-container'>
         <ImageSlider delay={4000} images={[
-          <img src={img1} alt='img1' />,
-          <img src={img2} alt='img2' />,
-          <img src={img3} alt='img3' />,
-          <img src={img4} alt='img4' />,
-          <img src={img5} alt='img5' />,
-          <img src={img6} alt='img6' />,
-          <img src={img7} alt='img7' />
+          <img src={img1} alt='img1' id="slide-image" />,
+          <img src={img2} alt='img2' id='slide-image'/>,
+          <img src={img3} alt='img3' id='slide-image'/>,
+          <img src={img4} alt='img4' id='slide-image'/>,
+          <img src={img5} alt='img5' id='slide-image'/>,
+          <img src={img6} alt='img6' id='slide-image'/>,
+          <img src={img7} alt='img7' id='slide-image'/>
         ]} />
       </div>
       <div className='join-program'>
