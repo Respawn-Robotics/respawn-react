834d16680db6f6958ee0929dbff8d777021d26ec
Exploding Pineapple <explodingpineapple41@gmail.com>
2023-01-13 13:37:05 -0500

    Started cloud functions

---
+++ functions/index.js
@@ -0,0 +1,58 @@
+/* eslint-disable */
+const functions = require("firebase-functions");
+
+const admin = require("firebase-admin");
+admin.initializeApp();
+// // Create and deploy your first functions
+// // https://firebase.google.com/docs/functions/get-started
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+exports.getAverages = functions.https.onCall((data, context) => {
+    let sortedList = {};
+    let avg = {};
+
+    data.map(entry => {
+        if (!sortedList[entry.team]) {
+            sortedList[entry.team] = [entry];
+            avg[entry.team] = {};
+            return;
+        }
+
+        sortedList[entry.team].push(entry);
+    })
+
+    Object.keys(sortedList).map(team => {
+        sortedList[team].map(entry => {
+            reconfig['data'].map(field => {
+                if (field.name === 'team' || field.name === 'match') return;
+                switch (field.type) {
+                    case 'number':
+                        if (!avg[team][field.name]) {
+                            avg[team][field.name] = entry[field.name] / sortedList[team].length;
+                            return;
+                        }
+                        avg[team][field.name] += entry[field.name] / sortedList[team].length;
+                        return;
+                    case 'select':
+                        const mappedOptions = field.options.reduce((acc, _, i) => {
+                            acc[field.options[i]] = i;
+                            return acc;
+                        }, {});
+
+                        if (!avg[team][field.name]) {
+                            avg[team][field.name] = mappedOptions[entry[field.name]] / sortedList[team].length;
+                            return;
+                        }
+                        avg[team][field.name] += mappedOptions[entry[field.name]] / sortedList[team].length;
+                        return;
+                    default:
+                        return;
+                }
+            })
+        })
+    })
+});
--- src/pages/recon/master-table/MasterTable.js
+++ src/pages/recon/master-table/MasterTable.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { getFunctions, httpsCallable } from "firebase/functions";
 import './master-table.css';
 
 import db from '../../../firebase.config';
@@ -7,46 +8,89 @@ import { onSnapshot, collection } from 'firebase/firestore';
 
 function MasterTable() {
     const [data, setData] = useState([]);
-    const [avg, setAvg] = useState({});
+    const [averages, setAverages] = useState({});
 
+    const functions = getFunctions();
+    const getAverages = httpsCallable(functions, 'getAverages');
+    getAverages({ team: 'all' })
+        .then((result) => {
+            console.log(result.data);
+        });
     useEffect(_ =>
         onSnapshot(collection(db, "recon"), snapshot =>
             setData(snapshot.docs.map(doc => doc.data()))), []);
 
     useEffect(_ => {
         let sortedList = {};
+        let avg = {};
 
         data.map(entry => {
             if (!sortedList[entry.team]) {
                 sortedList[entry.team] = [entry];
+                avg[entry.team] = {};
                 return;
             }
 
             sortedList[entry.team].push(entry);
         })
+
+        Object.keys(sortedList).map(team => {
+            sortedList[team].map(entry => {
+                reconfig['data'].map(field => {
+                    if (field.name === 'team' || field.name === 'match') return;
+                    switch (field.type) {
+                        case 'number':
+                            if (!avg[team][field.name]) {
+                                avg[team][field.name] = entry[field.name] / sortedList[team].length;
+                                return;
+                            }
+                            avg[team][field.name] += entry[field.name] / sortedList[team].length;
+                            return;
+                        case 'select':
+                            const mappedOptions = field.options.reduce((acc, _, i) => {
+                                acc[field.options[i]] = i;
+                                return acc;
+                            }, {});
+
+                            if (!avg[team][field.name]) {
+                                avg[team][field.name] = mappedOptions[entry[field.name]] / sortedList[team].length;
+                                return;
+                            }
+                            avg[team][field.name] += mappedOptions[entry[field.name]] / sortedList[team].length;
+                            return;
+                        default:
+                            return;
+                    }
+                })
+            })
+        })
+
+        setAverages(avg);
     }, [data])
-    
+
+    useEffect(_ => console.log(averages), [averages])
+
     return (
         <>
             <table id='master-table'>
                 <thead>
                     <tr className='master-table-row'>
-                        {Object.keys(data[0] ? data[0] : {}).map((field) => {
+                        <th className='master-table-cell master-table-heading'>Team</th>
+                        {Object.keys(averages[Object.keys(averages)[0]] ? averages[Object.keys(averages)[0]] : {}).map((field) => {
                             return (
-                                <th className='master-table-cell master-table-heading'>{field.toString()}</th>
+                                <th className='master-table-cell master-table-heading'>
+                                    {field.replace(/(-|_)+/g, " ").toLowerCase().replace(/(^|\s)[a-z]/g, (c) => c.toUpperCase())}
+                                </th>
                             );
                         })}
                     </tr>
                 </thead>
                 <tbody>
-                    {data.map((entry) => {
+                    {Object.keys(averages).map(team => {
                         return (
-                            <tr className='master-table-row' id={entry.id}>
-                                {Object.keys(entry).map((field) => {
-                                    return (
-                                        <td className='master-table-cell'>{entry[field].toString()}</td>
-                                    );
-                                })}
+                            <tr className='master-table-row' id={team.id}>
+                                <td className='master-table-cell'>{team}</td>
+                                {Object.keys(averages[team]).map(a => <td className='master-table-cell'>{averages[team][a]}</td>)}
                             </tr>
                         )
                     })}
