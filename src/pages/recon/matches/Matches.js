eaa3a143dc1be380cede031dfcf851a8e742b4a2
Exploding Pineapple <explodingpineapple41@gmail.com>
2023-01-28 15:43:44 -0500

    Need to cache api response

--- src/pages/recon/matches/Matches.js
+++ src/pages/recon/matches/Matches.js
@@ -1,44 +1,57 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import './matches.css';
 
 import db from '../../../firebase.config';
-import { onSnapshot, doc, getDoc } from 'firebase/firestore';
-
+import { onSnapshot, doc } from 'firebase/firestore';
+import { assertObjectTypeInternalSlot } from "@babel/types";
 
 function Matches() {
-    const [matches, setMatches] = useState({});
+    const [allMatchesData, setAllMatchesData] = useState()
+    const [matchesPlayed, setMatchesPlayed] = useState({});
+    const [matchNo, setMatchNo] = useState(0);
 
     useEffect(_ =>
         onSnapshot(doc(db, 'recon', 'entries'), doc => {
             const docData = doc.data();
             let tempMatches = {};
-            console.log(docData)
 
             Object.keys(docData).map(teamNumber => {
                 docData[teamNumber].map(teamMatch => {
-                    console.log(teamMatch)
-                    // teamMatch['team'] = teamNumber;
-                    // tempMatches[teamMatch.match] = [...tempMatches[teamMatch.match], teamMatch];
+                    teamMatch['team'] = teamNumber;
+                    tempMatches[teamMatch.match] = tempMatches[teamMatch.match] ? [...tempMatches[teamMatch.match], teamMatch] : [teamMatch];
                 });
             });
 
-            console.log(tempMatches);
+            setMatchesPlayed(tempMatches);
         }), []);
 
-    const getMatches = _ => {
-        const xhr = new XMLHttpRequest();
+    async function fetchMatches() {
+        const apiCall = fetch('https://www.thebluealliance.com/api/v3/event/2022paca/matches', {
+            cache: 'force-cache',
+            method: 'GET',
+            headers: {
+                'X-TBA-Auth-Key': process.env.REACT_APP_tbaKey
+            }
+        })
+            .then(response => response.json())
+            .then(json => json
+                .filter(match => match['comp_level'] === 'qm')
+                .map(match => Object.keys(match['alliances'])
+                    .map(alliance => { return { [alliance]: match['alliances'][alliance]['team_keys'] } })
+                ));
 
-        xhr.addEventListener('load', _ => {
-            console.log(JSON.parse(xhr.responseText));
-        });
-
-        xhr.open('GET', 'https://www.thebluealliance.com/api/v3/team/frc325/event/2022paca/matches');
-        xhr.setRequestHeader('X-TBA-Auth-Key', process.env.REACT_APP_tbaKey);
-        xhr.send();
+        return await apiCall;
     }
 
+    const DisplayMatch = _ => <>
+    
+    </>
+
+    const changeMatchNo = e => setMatchNo(parseInt(e.target.value ? e.target.value : 0));
+
     return <>
-        <input type='number' />
+        <input type='number' onChange={changeMatchNo} />
+        <DisplayMatch />
     </>;
 }
 
