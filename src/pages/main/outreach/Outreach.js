a5a224a0c8cee7a0af4a24f4fbac25f65cb6d43e
Exploding Pineapple <explodingpineapple41@gmail.com>
2023-01-30 19:33:07 -0500

    Added FIRST page and fixed Outreach CSS

--- src/pages/main/outreach/Outreach.js
+++ src/pages/main/outreach/Outreach.js
@@ -19,11 +19,9 @@ function Outreach() {
                     <img src={reach8} alt="Respawn Robotics Outreach" />
                 </ImageSlider>
             </div>
-            <div className='intro-title'>
-                <h1 className='title-text'>Respawn Robotics is deeply rooted in its community</h1>
-            </div>
-            <div className='intro-text'>
-                <p className='regular-text'>
+            <div id='outreach-intro'>
+                <h1>Respawn Robotics is deeply rooted in its community</h1>
+                <p>
                     Respawn Robotics participates in a variety of outreach
                     activities that benefit the community, and inspire young
                     leaders to gain an appreciation and interest in STEM fields.
@@ -31,31 +29,35 @@ function Outreach() {
                     and STEM.
                 </p>
             </div>
-            <div className='revolution'>
-                <h2 className='leftSmaller-title-text'>An Education Revolution</h2>
-                <hr className='leftSeparator' />
-                <p className='leftRegular-text'>
-                    We are honored to have the opportunity to host over 3000 eighth graders
-                    in our lab throughout the school year. With a goal to interest them
-                    in pursuing the mechatronics career field and pursue STEM.
-                </p>
-                <div className='topImage-placeholder'>
-                    <img src={reach5} alt="Respawn Robotics Outreach" className="right-lower-image" />
+            <div className='outreach-container'>
+                <div className='text-content'>
+                    <h2>An Education Revolution</h2>
+                    <hr />
+                    <p>
+                        We are honored to have the opportunity to host over 3000 eighth graders
+                        in our lab throughout the school year. With a goal to interest them
+                        in pursuing the mechatronics career field and pursue STEM.
+                    </p>
+                </div>
+                <div className='image-content'>
+                    <img src={reach5} alt="Respawn Robotics Outreach" />
                 </div>
             </div>
-            <div className='inspiring'>
-                <div className='bottomImage-placeholder'>
-                    <img src={reach11} alt="Respawn Robotics Outreach" className='left-lower-image' />
+            <div className='outreach-container'>
+                <div className='image-content'>
+                    <img src={reach11} alt="Respawn Robotics Outreach" />
                 </div>
-                <h2 className='rightSmaller-title-text'>Inspiring the Next Generation</h2>
-                <hr className='rightSeparator' />
-                <p className='rightRegular-text'>
-                    We are proud to introduce our program that takes <i>FIRST</i> not
-                    only to middle schools, but to the elementary schools! Our first event
-                    was at Wildwood Elemenatry School where we had the opportunity to
-                    inspire the younger generation and get them integrated into STEM
-                    and <i>FIRST</i>. We were able to reach over 500 students with one event.
-                </p>
+                <div className='text-content'>
+                    <h2>Inspiring the Next Generation</h2>
+                    <hr />
+                    <p>
+                        We are proud to introduce our program that takes <i>FIRST</i> not
+                        only to middle schools, but to the elementary schools! Our first event
+                        was at Wildwood Elemenatry School where we had the opportunity to
+                        inspire the younger generation and get them integrated into STEM
+                        and <i>FIRST</i>. We were able to reach over 500 students with one event.
+                    </p>
+                </div>
             </div>
         </>
     );
