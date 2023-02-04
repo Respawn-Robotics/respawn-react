727884cc9c67c558a75bcf6b683e81579b86bfa9
gitJhim <nohaxjustchickn@gmail.com>
2023-02-04 12:40:48 -0800

    added delete team

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,53 +1,8 @@
 import React, { useEffect, useState } from "react";
 import './user.css';
-import { query, collection, where, getDocs, doc, getDoc, onSnapshot, setDoc, updateDoc, arrayUnion, arrayRemove } from 'firebase/firestore';
-import db from '../../firebase.config'
-import { useNavigate } from 'react-router-dom'
-import { toast } from 'react-toastify';
 
-function Users({currentUserRank, userData, rank, scoutData}) {
+function Users({currentUserRank, userData, rank, scoutData, kickUser, promoteUser, demoteUser}) {
     const [userScouts, setUserScouts] = useState([]);
-    const teamDocRef = doc(db, "teams", userData.team);
-    const navigate = useNavigate()
-    const [rankState, setRankState] = useState("")
-    const usersDocRef = doc(db, "users", userData.uid);
-
-    useEffect(_ => {
-        setRankState(rank)
-    }, [rank])
-
-    const promoteUser = uid => {
-        updateDoc(teamDocRef, {
-            admins: arrayUnion(uid)
-        })
-        navigate('/recon/manage-team')
-        toast("Successfuly promoted " + userData.displayName + "!")
-    }
-
-    const demoteUser = uid => {
-        updateDoc(teamDocRef, {
-            admins: arrayRemove(uid)
-        })
-        navigate('/recon/manage-team')
-        toast("Successfuly demoted " + userData.displayName + "!")
-    }
-
-    const kickUser = uid => {
-        updateDoc(teamDocRef, {
-            admins: arrayRemove(uid)
-        })
-
-        updateDoc(teamDocRef, {
-            users: arrayRemove(uid)
-        })
-
-        updateDoc(usersDocRef, {
-            team: ""
-        })
-
-        navigate('/recon/manage-team')
-        toast("Successfuly kicked " + userData.displayName + "!")
-    }
 
     useEffect(_ => {
         let tempArray = 0;
@@ -65,8 +20,8 @@ function Users({currentUserRank, userData, rank, scoutData}) {
             {(currentUserRank == "Owner") && 
             <>
                 {(rank != "Owner") && <> 
-                {(rank == "Admin") && <button className='manage-user' onClick={_ => demoteUser(userData.uid)}>Demote User</button>}
-                {(rank != "Admin") && <button className='manage-user' onClick={_ => promoteUser(userData.uid)}>Promote User</button>} 
+                {(rank == "Admin") && <button className='manage-user' onClick={_ => demoteUser(userData.uid, userData)}>Demote User</button>}
+                {(rank != "Admin") && <button className='manage-user' onClick={_ => promoteUser(userData.uid, userData)}>Promote User</button>} 
                 <button className='manage-user' onClick={_ => kickUser(userData.uid)}>Kick User</button>
                 </>}
             </>}            
