d6447fd7d52bc86aa341c717953227550d1c7e25
Dev Bhagat <explodingpineapple41@gmail.com>
2023-02-06 17:00:41 -0500

    Added all the text to Respawn Recon Pillar page

--- src/pages/main/pillars/respawn-recon/RespawnRecon.js
+++ src/pages/main/pillars/respawn-recon/RespawnRecon.js
@@ -1,14 +1,15 @@
 import React from "react";
 import './respawn-recon.css';
 
-import ReconUI from './media/recon-ui.png'
+import ReconUI from './media/recon-ui.png';
+import TeamsUI from './media/teams-ui.png';
 
 function RespawnRecon() {
     return <>
         <h1 id='recon-intro'>Our World Class Scouting App: <a href='/recon'>Respawn Recon</a></h1>
         <div className="recon-feature-container">
             <div className="feature-image">
-                <img src={ReconUI} />
+                <img className="vertical-image" src={ReconUI} />
             </div>
             <div className="feature-text">
                 <p>Intuitive and visually appealing scouting form</p>
@@ -20,24 +21,35 @@ function RespawnRecon() {
             </div>
         </div>
         <div className="recon-feature-container">
-
-        </div>
-        <div className="recon-feature-container">
-            <div className="recon-feature-container">
-                <div className="feature-image">
-                    <img src={ReconUI} />
-                </div>
-                <div className="feature-text">
-                    <p>Hosting of all scouting data online in the cloud</p>
-                    <ul>
-                        <li>View all of your team's uploaded scouts in one place</li>
-                        <li>Fast searching for individual robot data</li>
-                    </ul>
-                </div>
+            <div className="feature-text">
+                <p>Capabilities for both online and offline scouting</p>
+                <ul>
+                    <li>Ability to upload instantly if internet is available</li>
+                    <li>Download JSON file containing scout data and upload to database once internet is available</li>
+                </ul>
             </div>
         </div>
         <div className="recon-feature-container">
-
+            <div className="feature-image">
+                <img className="horizontal-image" src={TeamsUI} />
+            </div>
+            <div className="feature-text">
+                <p>Hosting of all scouting data online in the cloud</p>
+                <ul>
+                    <li>View all of your team's uploaded scouts in one place</li>
+                    <li>Fast searching for individual robot data</li>
+                </ul>
+            </div>
+        </div>
+        <div className="recon-feature-container">
+        <div className="feature-text">
+                <p>Account management system for creating and deleting scouts</p>
+                <ul>
+                    <li>Create an account and scouting team</li>
+                    <li>Invite users to team and promote / demote them to admins</li>
+                    <li>Review scouts and delete bad data</li>
+                </ul>
+            </div>
         </div>
     </>
 }
