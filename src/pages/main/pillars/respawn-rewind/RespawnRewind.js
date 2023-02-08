42b1fe9be0e7e4b7d726e28b47c3c5dd62e713e5
Dev Bhagat <explodingpineapple41@gmail.com>
2023-02-08 13:49:23 -0500

    Added template for repository and rewind

--- src/pages/main/pillars/respawn-rewind/RespawnRewind.js
+++ src/pages/main/pillars/respawn-rewind/RespawnRewind.js
@@ -4,7 +4,21 @@ import Alum from "../../../../components/alum/Alum";
 
 function RespawnRewind() {
     return <>
-        <h1>WIP</h1>
+        <div id='rewind-intro'>
+            <h1>Respawn Rewind</h1>
+            <h3>Where they are now</h3>
+        </div>
+        <div id='alumns-container'>
+            <Alum 
+                content={{
+                    name: 'Dev Bhagat',
+                    titles: ['CTO'],
+                    desc: "BEST IN THE GAME",
+                    year: 2023
+                }}
+                image='https://th.bing.com/th/id/OIP.uKKrotWMvoZMBTZD9b_e7wHaEK?w=283&h=180&c=7&r=0&o=5&dpr=1.8&pid=1.7'
+            />
+        </div>
     </>
 }
 
