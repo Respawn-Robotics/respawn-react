b0d2498e7cee27197712e88c322d588fc4b9836d
Dev Bhagat <explodingpineapple41@gmail.com>
2023-02-08 14:57:46 -0500

    Finished templates for repository and rewind

--- src/pages/main/pillars/respawn-rewind/RespawnRewind.js
+++ src/pages/main/pillars/respawn-rewind/RespawnRewind.js
@@ -8,16 +8,17 @@ function RespawnRewind() {
             <h1>Respawn Rewind</h1>
             <h3>Where they are now</h3>
         </div>
-        <div id='alumns-container'>
-            <Alum 
+            <h1 style={{color: 'red', fontFamily: 'roboto-slab', textAlign: 'center', fontSize: '3rem'}}>FUTURE ALUMS WILL BE POSTED HERE</h1>
+        <div id='alumnis-container'>
+            {/* <Alum 
                 content={{
                     name: 'Dev Bhagat',
                     titles: ['CTO'],
-                    desc: "BEST IN THE GAME",
+                    desc: "BEST IN THE GAME BEST IN THE GAME BEST IN THE GAME BEST IN THE GAME BEST IN THE GAME BEST IN THE GAME BEST IN THE GAME BEST IN THE GAME BEST IN THE GAME BEST IN THE GAME BEST IN THE GAME BEST IN THE GAME BEST IN THE GAME ",
                     year: 2023
                 }}
-                image='https://th.bing.com/th/id/OIP.uKKrotWMvoZMBTZD9b_e7wHaEK?w=283&h=180&c=7&r=0&o=5&dpr=1.8&pid=1.7'
-            />
+                image='https://th.bing.com/th/id/OIP.YQqrNNzp_SRwlkaDUaLLwgHaEo?w=269&h=180&c=7&r=0&o=5&dpr=1.8&pid=1.7'
+            /> */}
         </div>
     </>
 }
